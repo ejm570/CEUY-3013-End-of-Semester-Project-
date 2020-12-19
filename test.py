bba50c1c17bbaa6855376839a6a1695ff4671fad
ejm570 <73856285+ejm570@users.noreply.github.com>
2020-12-19 18:09:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from source import System
+import matplotlib.pyplot as plt
 ##spipe = suction pipe
 ##dpipe = discharge pipe
 ##to test code create vectors spipe, dpipe
@@ -11,7 +12,7 @@ dpipe = (3/12, 250, 100, 2.8)
 reservoirs = (-13, 75)
 x = System(spipe, dpipe, reservoirs, 4)
 ##functions that generate primary output
-plt = x.system_curve(90)
+x.system_curve(90)
 x.impeller_size(90)
 x.valid_loc(90)
 ##intermediate functions
@@ -20,5 +21,5 @@ print(x.head_loss(90))
 print(x.head_req(90))
 print(x.NPSH_available(90))
 print(x.NPSH_required(90))
-##following line prevents the plot from blocking code that comes after plt.show
+##following line shows plot generated in system_curve function
 plt.show()
